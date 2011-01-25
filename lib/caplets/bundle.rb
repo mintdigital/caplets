6a46e8241b68eddf1d40a7431f93884c8d2ffdde
Dean Strelau <dean@mintdigital.com>
2011-01-25 15:47:53 -0500

    Deprecate caplets/bundle

--- lib/caplets/bundle.rb
+++ lib/caplets/bundle.rb
@@ -3,12 +3,19 @@ set :bundle_exclude, %w[development test]
 set :bundle_to, 'vendor/bundled_gems'
 set :bin_cmd, 'bundle exec'
 
+warn "**************************************************************"
+warn "[DEPRECATED] caplets/bundle is deprecated."
+warn "`require bundler/capistrano` and use 'bundle:install' instead."
+warn 'See http://gembundler.com/deploying.html for more information.'
+warn "**************************************************************"
+
 ## Tasks
 namespace :deploy do
   namespace :bundle do
     desc "Install gems from Gemfile"
     task :install, :roles => lambda { fetch(:bundle_roles) },
                    :except => {:no_release => true} do
+
       without = fetch(:bundle_exclude).map{|g| "--without #{g}"}.join(' ')
       run_current "bundle check || bundle install #{fetch(:bundle_to)} #{without}"
     end
