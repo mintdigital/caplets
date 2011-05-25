d6a996f5cefd1b9b9b6dc2456654286f59032564
Dean Strelau <dean@mintdigital.com>
2011-05-25 16:51:47 -0400

    release 1.0.3

--- lib/caplets/version.rb
+++ lib/caplets/version.rb
@@ -3,7 +3,7 @@ module Caplets
     MAJOR = 1
     MINOR = 0
     TINY  = 3
-    BUILD = 'pre'
+    BUILD = nil
     STRING = [MAJOR,MINOR,TINY,BUILD].compact.join('.')
   end
   VERSION = Version::STRING
